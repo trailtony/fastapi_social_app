c15a433155ae03a42e33917c173f0183ac3eac33
trailtony <faithmakerneo@gmail.com>
2025-11-04 17:56:06 +0000

    Added Save information to DataBase functionality

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -13,36 +13,16 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-text_posts = {
-    1: {"title": "New Post", "content": "Cool test post"}, 
-    2: {"title": "Python Tip", "content": "Use list comprehensions for cleaner loops."}, 
-    3: {"title": "Daily Motivation", "content": "Consistency beats intensity every time."}, 
-    4: {"title": "Fun Fact", "content": "The first computer bug was an actual moth found in a Harvard Mark II."}, 
-    5: {"title": "Update", "content": "Just launched my new project! Excited to share more soon."}, 
-    6: {"title": "Tech Insight", "content": "Async IO in Python can massively speed up I/O bound tasks."}, 
-    7: {"title": "Quote", "content": "Programs must be written for people to read, and only incidentally for machines."}, 
-    8: {"title": "Weekend Plans", "content": "Might finally clean up my GitHub repos... or just play some Minecraft."}, 
-    9: {"title": "Question", "content": "What's the most underrated Python library you've ever used?"}, 
-    10: {"title": "Mini Announcement", "content": "New video drops tomorrow covering the weirdest Python features!"}
-}
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...), caption: str = Form(""), session: AsyncSession = Depends(get_async_session)) -> Post:
+    post = Post(
+        caption=caption, 
+        url="dummy url", 
+        file_type = "photo",
+        file_name = "dummy name"
+    )
 
-@app.get("/posts")
-def get_all_posts(limit: int = None):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
-
-
-@app.get("/posts/{id}")
-def get_post(id: int) -> PostResponse:
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail="Post not found")
-    
-    return text_posts.get(id)
-
-
-@app.post("/posts")
-def create_post(post: PostCreate) -> PostResponse:
-    new_post = {"title": post.title, "content": post.content}
-    text_posts[max(text_posts.keys()) + 1] = new_post
-    return new_post
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post

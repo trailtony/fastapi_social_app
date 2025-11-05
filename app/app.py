e0cbf9894ef3f66b07eb6326b152fb16ef7d60fe
trailtony <still_running_uphill@protonmail.com>
2025-11-05 19:39:48 +0000

    Added data retrieval endpoint get_feed.

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -14,7 +15,12 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/upload")
-async def upload_file(file: UploadFile = File(...), caption: str = Form(""), session: AsyncSession = Depends(get_async_session)) -> Post:
+async def upload_file(
+        file: UploadFile = File(...), 
+        caption: str = Form(""), 
+        session: AsyncSession = Depends(get_async_session)
+):
+    
     post = Post(
         caption=caption, 
         url="dummy url", 
@@ -26,3 +32,23 @@ async def upload_file(file: UploadFile = File(...), caption: str = Form(""), ses
     await session.commit()
     await session.refresh(post)
     return post
+
+
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+    
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
+    return {"posts": posts_data}

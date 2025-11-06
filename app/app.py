c39f8718298a3ca7b8100db5882f9572a9d877d5
trailtony <faithmakerneo@gmail.com>
2025-11-06 23:57:04 +0000

    Added image and video upload through imagekit API

--- app/app.py
+++ app/app.py
@@ -4,6 +4,15 @@ from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from contextlib import asynccontextmanager
+from app.images import imagekit
+from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+import shutil
+import os
+import uuid
+import tempfile
+
+
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,18 +29,37 @@ async def upload_file(
         caption: str = Form(""), 
         session: AsyncSession = Depends(get_async_session)
 ):
-    
-    post = Post(
-        caption=caption, 
-        url="dummy url", 
-        file_type = "photo",
-        file_name = "dummy name"
-    )
+    temp_file_path = None
 
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
-    return post
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+
+        upload_result = imagekit.upload_file(
+            file=open(temp_file_path, "rb"), file_name=file.filename, options=UploadFileRequestOptions(
+                use_unique_file_name=True, tags=["backend-upload"]
+            )
+        )
+
+        if upload_result.response_metadata.http_status_code == 200:
+            post = Post(
+                caption=caption, 
+                url=upload_result.url, 
+                file_type = "video" if file.content_type.startswith("video/") else "image",
+                file_name = upload_result.name
+            )
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+            return post
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
 
 
 @app.get("/feed")
